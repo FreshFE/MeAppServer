5c269764a13d0215e4c3eced2bfd7ba2e4372171
MinoWu <minowu@foxmail.com>
2014-03-29 17:05:53 +0800

    创建MeAppPlatformBundle

---
+++ src/MeApp/PlatformBundle/Entity/platform.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace MeApp\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * platform
+ */
+class platform
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return platform
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
