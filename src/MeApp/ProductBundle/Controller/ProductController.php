781524d21997c3f455addb986f34de2610bd3173
MinoWu <minowu@foxmail.com>
2014-03-29 20:05:37 +0800

    创建Product相关的controller, index, show, test文件

---
+++ src/MeApp/ProductBundle/Controller/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MeApp\ProductBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ProductController extends Controller
+{
+    public function indexAction()
+    {
+    }
+
+    public function showAction($productId)
+    {
+    }
+
+}
