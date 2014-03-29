f5fe081c18ef5fe3a82659e270d2cdf1bad90878
MinoWu <minowu@foxmail.com>
2014-03-30 00:36:00 +0800

    添加$class属性和create方法

--- src/MeApp/CoreBundle/EntityManager/BaseManager.php
+++ src/MeApp/CoreBundle/EntityManager/BaseManager.php
@@ -10,15 +10,24 @@ class BaseManager
 
 	protected $repository;
 
+	protected $class;
+
 	public function __construct(EntityManager $em, $entityName)
 	{
 		$this->em = $em;
 
 		$this->repository = $this->em->getRepository($entityName);
+
+		$this->class = $this->em->getClassMetadata($entityName)->getName();
 	}
 
 	public function getRepository()
 	{
 		return $this->repository;
+	}
+
+	public function create()
+	{
+		return new $this->class;
 	}
 }
