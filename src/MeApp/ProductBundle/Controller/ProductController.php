3cf5891dd0c696b519c887924b9eabee1649e644
MinoWu <minowu@foxmail.com>
2014-03-29 22:38:31 +0800

    编辑ProductController

--- src/MeApp/ProductBundle/Controller/ProductController.php
+++ src/MeApp/ProductBundle/Controller/ProductController.php
@@ -2,16 +2,26 @@
 
 namespace MeApp\ProductBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use MeApp\CoreBundle\Controller\BaseController;
 
-class ProductController extends Controller
+class ProductController extends BaseController
 {
+    protected $bundleName = 'MeAppProductBundle';
+
     public function indexAction()
     {
     }
 
     public function showAction($productId)
     {
+        $product = $this
+            ->get('app.db.product')
+            ->getRepository()
+            ->find($productId);
+
+    	return $this->render(
+                $this->buildTemplate('Product:index')
+            );
     }
 
 }
